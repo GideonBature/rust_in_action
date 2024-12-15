7939b69d0a6bec45311da5035b2fdb434ba2c87f
GideonBature <infoaboutgideon@gmail.com>
2024-12-15 14:01:00 +0100

    Feat: testing function

--- generics_with_trait_bounds/src/main.rs
+++ generics_with_trait_bounds/src/main.rs
@@ -1,10 +1,19 @@
 use std::ops::{Add};
+use std::time::{Duration};
 
 fn add<T: Add<Output = T>>(i: T, j: T) -> T {
     i + j
 }
 
 fn main() {
-    let ans = add(-14.5, 7.8);
-    println!("add: {}", ans);
+    let floats = add(1.2, 3.6);
+    let ints = add(10, 50);
+    let durations = add(
+        Duration::new(5, 0),
+        Duration::new(10, 0)
+    );
+
+    println!("{}", floats);
+    println!("{}", ints);
+    println!("{:?}", durations);
 }
