d58cb1bc50db63cc448ba89ec411edda0bead5fe
GideonBature <infoaboutgideon@gmail.com>
2024-12-19 08:47:41 +0100

    Feat: reading line by line

---
+++ read_file_line/readme.md
@@ -0,0 +1,5 @@
+# My Name is Rust
+## I am a System Programming Language
+### For the past eight (8) years, I have been the love of so many persons.
+## How do you think of me when you think of a programming language?
+# Don't you think I am more than capable to handle all of your memory issues?
--- read_file_line/src/main.rs
+++ read_file_line/src/main.rs
@@ -1,5 +1,13 @@
-
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("readme.md").unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{} ({} bytes long)", line, line.len());
+    }
 }
